8a13887eceaa61fc3f16d27d869b7282519f9867
yeimyhs <67721046+yeimyhs@users.noreply.github.com>
2021-08-20 20:37:12 -0500

    se agrego los permisos

--- views.py
+++ views.py
@@ -50,6 +50,7 @@ class UserObtainAuthToken(APIView):
 
 # Register API
 class RegisterAPI(generics.GenericAPIView):
+    permission_classes = (permissions.AllowAny,)
     serializer_class = RegisterSerializer
 
     def post(self, request, *args, **kwargs):
