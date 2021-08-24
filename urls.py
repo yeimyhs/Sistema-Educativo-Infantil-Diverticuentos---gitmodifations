84b01caaa73ff769e4d614c2e723cad35210ddd5
yeimyhs <67721046+yeimyhs@users.noreply.github.com>
2021-08-24 13:29:00 -0500

    edicion de urls swagger

--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from DivertiCuentos import views
 from knox import views as knox_views
 #from .views import LoginAPI
 from .views import *
-from django.urls import path
+from django.urls import path , re_path
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -48,10 +48,9 @@ urlpatterns = [
 
     path('userbyToken/', UserAPI.as_view()),
 
-    path(r'swagger(<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/auth/', include('knox.urls')),
+    re_path(r'^swagger(<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 router = SimpleRouter()
 
