ba4146f759923ac9f03e161fbf8c8b7e0b72ca0b
yeimyhs <67721046+yeimyhs@users.noreply.github.com>
2021-09-02 17:20:11 -0500

    se registro el nuevo user en admin

--- admin.py
+++ admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+#admin.site.register(Usuario)
+admin.site.register(UserP)
+#admin.site.register(Cuento)
+#admin.site.register(Comentarios)
+#admin.site.register(UsuarioGrupos)
